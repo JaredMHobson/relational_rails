384a56a2f5cf8bac3ccbf7f5229573e638eb3284
Jared Hobson <jaredmhobson@gmail.com>
2024-03-21 11:14:31 -0700

    Add test to show developer name on developers/show page

--- spec/features/developers/show_spec.rb
+++ spec/features/developers/show_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'the developers show page' do
-  it '' do
+  before :each do
+    @developer1 = Developer.create!(name: "Nintendo", is_indie: false, year_founded: 1889)
+    @developer2 = Developer.create!(name: "Valve Corporation", is_indie: false, year_founded: 1996)
+  end
+
+  it 'displays the name of the developer' do
+    visit "/developers/#{@developer1.id}"
+
+    expect(page).to have_content(@developer1.name)
+
+    visit "/developers/#{@developer2.id}"
+
+    expect(page).to have_content(@developer2.name)
   end
 end
