624087c14bc2387d30386916dbe167c89e6a6d81
Jared Hobson <jaredmhobson@gmail.com>
2024-03-21 11:10:48 -0700

    Update instantiations in developers/index_spec

--- spec/features/developers/index_spec.rb
+++ spec/features/developers/index_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'the developers index page' do
-  it 'displays all developers' do
-    developer1 = Developer.create!(name: "Nintendo", is_indie: false, year_founded: 1889)
-    developer2 = Developer.create!(name: "Valve Corporation", is_indie: false, year_founded: 1996)
-    developer3 = Developer.create!(name: "Playtonic Games", is_indie: true, year_founded: 2014)
+  before :each do
+    @developer1 = Developer.create!(name: "Nintendo", is_indie: false, year_founded: 1889)
+    @developer2 = Developer.create!(name: "Valve Corporation", is_indie: false, year_founded: 1996)
+    @developer3 = Developer.create!(name: "Playtonic Games", is_indie: true, year_founded: 2014)
+  end
 
+  it 'displays all developers names' do
     visit "/developers"
 
-    expect(page).to have_content(developer1.name)
-    expect(page).to have_content(developer2.name)
-    expect(page).to have_content(developer3.name)
+    expect(page).to have_content(@developer1.name)
+    expect(page).to have_content(@developer2.name)
+    expect(page).to have_content(@developer3.name)
   end
 end
