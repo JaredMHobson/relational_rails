862265dbca83fe8d0bbaba9bfddeb99ecfc939af
Jared Hobson <jaredmhobson@gmail.com>
2024-03-22 01:00:39 -0700

    Add test for showing games count on developers/show_spec

--- spec/features/developers/show_spec.rb
+++ spec/features/developers/show_spec.rb
@@ -15,4 +15,20 @@ RSpec.describe 'the developers show page' do
 
     expect(page).to have_content(@developer2.name)
   end
+
+  describe 'User Story 7' do
+    it 'display a count of the number of games made by the developer' do
+      @mario = @developer1.games.create!(name: "Super Mario Galaxy", has_multiplayer: false, year_released: 2050)
+      @zelda = @developer1.games.create!(name: "Twilight Princess", has_multiplayer: false, year_released: 1999)
+      @tf2 = @developer2.games.create!(name: "Team Fortress 2", has_multiplayer: true, year_released: 2009)
+
+      visit "/developers/#{@developer1.id}"
+
+      expect(page).to have_content("Total Games: #{@developer1.game_count}")
+
+      visit "/developers/#{@developer2.id}"
+
+      expect(page).to have_content("Total Games: #{@developer2.game_count}")
+    end
+  end
 end
