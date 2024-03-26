2fdeaff704f6ed69d69ea4f453656b942d8c6a73
Jared Hobson <jaredmhobson@gmail.com>
2024-03-26 14:20:16 -0700

    Create ::sort_by_total_games method in developer

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -4,4 +4,10 @@ class Developer < ApplicationRecord
   def game_count
     games.count
   end
+
+  def self.sort_by_total_games
+    all.sort_by do |dev|
+      dev.game_count
+    end.reverse
+  end
 end
